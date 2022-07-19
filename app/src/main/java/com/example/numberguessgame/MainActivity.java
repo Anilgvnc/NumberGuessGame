bbc5d13e1fa45970c1f540925ba8327da41ade72
anil gvnc <anilnehir@hotmail.com>
2022-07-19 15:21:20 +0300

    initial commit

--- app/src/main/java/com/example/numberguessgame/MainActivity.java
+++ app/src/main/java/com/example/numberguessgame/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.numberguessgame;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -13,6 +14,8 @@ import java.util.Random;
 public class MainActivity extends AppCompatActivity {
 
     int randomNum;
+    MediaPlayer correctEffect;
+    MediaPlayer wrongEffect;
 
     public void Guess(View view){
         EditText guess = (EditText) findViewById(R.id.numberTool);
@@ -20,12 +23,15 @@ public class MainActivity extends AppCompatActivity {
 
         if(guessInt > randomNum){
             Toast.makeText(this, "Lower!", Toast.LENGTH_SHORT).show();
+            wrongEffect.start();
         }
         else if(guessInt < randomNum){
             Toast.makeText(this, "Higher!", Toast.LENGTH_SHORT).show();
+            wrongEffect.start();
         }
         else{
             Toast.makeText(this, "Correct! One more time :)", Toast.LENGTH_SHORT).show();
+            correctEffect.start();
 
             /*
              * generate a number 1-20
@@ -44,6 +50,9 @@ public class MainActivity extends AppCompatActivity {
 
         Random random = new Random();
         randomNum = random.nextInt(20) + 1;
+
+        wrongEffect = MediaPlayer.create(this, R.raw.wrong);
+        correctEffect = MediaPlayer.create(this, R.raw.correct);
 
     }
 }
